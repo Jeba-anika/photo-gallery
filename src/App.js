d17c803971db2c699b75d6bddd2944593b3b68bf
Jeba Anika <anikanur98@gmail.com>
2023-11-01 00:13:38 +0600

    photo drag and drop sorting done

--- src/App.js
+++ src/App.js
@@ -5,14 +5,42 @@ import { AiOutlinePicture } from "react-icons/ai";
 
 function App() {
   const [selectedImages, setSelectedImages] = useState([])
+  const [draggedImage, setDraggedImage] = useState(null)
+
+
+
+
   const handleSelectedImages = (event) => {
     const selectedFiles = event.target.files
     const selectedFilesArray = Array.from(selectedFiles)
-    console.log(selectedFiles)
-    console.log(selectedFilesArray)
+
     const imageArray = selectedFilesArray.map(image => URL.createObjectURL(image))
     setSelectedImages(imageArray)
   }
+
+
+
+  const handleDragStart = (e, item) => {
+    setDraggedImage(item);
+  };
+
+
+  const handleDragOver = (e, item) => {
+    if (draggedImage === null) return;
+    if (item === draggedImage) return;
+
+    const newItems = [...selectedImages];
+    const draggedIndex = selectedImages.indexOf(draggedImage);
+    const itemIndex = selectedImages.indexOf(item);
+  
+    newItems.splice(draggedIndex, 1);
+    newItems.splice(itemIndex, 0, draggedImage);
+
+    setSelectedImages(newItems);
+    setDraggedImage(null);
+  };
+
+
   return (
     <div>
       <div className='md:w-2/3 mx-3 md:mx-auto my-9 shadow min-h-screen rounded'>
@@ -23,20 +51,27 @@ function App() {
           {
             selectedImages &&
             selectedImages.map((image, index) =>
-              <img className={`${index===0 ? "col-span-2 row-span-2": "col-span-1 row-span-1"} border rounded`} src={image} alt='' />
+              <img
+               
+                draggable
+                onDragStart={(e) => handleDragStart(e, image)}
+                onDragOver={(e) => e.preventDefault()}
+                onDrop={(e) => handleDragOver(e, image)}
+                className={`${index === 0 ? "col-span-2 row-span-2" : "col-span-1 row-span-1"} border rounded`}
+                src={image} alt='' />
             )
           }
           <div>
-          <label className='border-dashed border p-5 rounded cursor-pointer'>
-            <span>
-              {/* <AiOutlinePicture /> */}
-              Add Images
-              <input type='file' name="images" multiple onChange={handleSelectedImages} accept='image/png , image/jpeg , image/jpg , image/webp' className='hidden ' />
-            </span>
-          </label>
+            <label className='border-dashed border p-5 rounded cursor-pointer'>
+              <span>
+                {/* <AiOutlinePicture /> */}
+                Add Images
+                <input type='file' name="images" multiple onChange={handleSelectedImages} accept='image/png , image/jpeg , image/jpg , image/webp' className='hidden ' />
+              </span>
+            </label>
+          </div>
         </div>
-        </div>
-        
+
 
       </div>
     </div>
